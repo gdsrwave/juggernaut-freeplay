8a43bc3fa58665918304b7b173b014a047194080
Jack Engle <wavegdsr@gmail.com>
2024-05-27 17:49:52 -0400

    rearrange helper scopes, remove tutorial comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
 #include <Geode/ui/BasedButtonSprite.hpp>
 #include <hjfod.gmd-api/include/GMD.hpp>
@@ -13,104 +10,57 @@
 #include <fmt/core.h>
 #include <Geode/cocos/support/zip_support/ZipUtils.h>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
+// bring used namespaces to scope
 using namespace geode::prelude;
 using namespace gmd;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature ofBASE_LEVEL = f'kS38,1_28_2_34_3_44_11_255_12_255_13_255_4_-1_6_1000_7_1_15_1_18_0_8_1|1_25_2_24_3_24_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_255_2_75_3_0_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_255_2_75_3_0_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|,kA13,0,kA15,0,kA16,0,kA14,,kA6,0,kA7,7,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,1,kA9,0,kA10,0,kA11,0;1,747,2,15,3,15,54,160;1,7,2,15,3,105,6,-90,21,1004;1,5,2,15,3,75,21,1004;1,5,2,15,3,45,21,1004;1,5,2,15,3,15,21,1004;1,7,2,45,3,105,6,-90,21,1004;1,5,2,45,3,15,21,1004;1,5,2,45,3,45,21,1004;1,1007,2,-15,3,285,20,1,36,1,51,2,10,1.57,35,0.5;1,5,2,45,3,75,21,1004;1,7,2,75,3,105,6,-90,21,1004;1,5,2,75,3,15,21,1004;1,5,2,75,3,75,21,1004;1,5,2,75,3,45,21,1004;1,7,2,195,3,15,21,1004;1,5,2,105,3,75,21,1004;1,5,2,105,3,15,21,1004;1,7,2,105,3,105,6,-90,21,1004;1,5,2,165,3,75;1,5,2,105,3,45,21,1004;1,103,2,165,3,129;1,5,2,135,3,75,21,1004;1,5,2,135,3,45,21,1004;1,5,2,135,3,15,21,1004;1,8,2,195,3,135;1,5,2,165,3,15,21,1004;1,5,2,165,3,45,21,1004;1,7,2,135,3,105,6,-90,21,1004;1,7,2,165,3,105,6,-90;1,1,2,195,3,105;1,7,2,195,3,75;1,7,2,195,3,45,21,1004;1,1338,2,255,3,45;1,660,2,255,3,163,6,17,13,0;1,{speed},2,255,3,165,13,1;1,1338,2,225,3,15;1,1338,2,285,3,{225+CORRIDOR_HEIGHT},6,270;1,1338,2,285,3,75;1,1338,2,345,3,{165+CORRIDOR_HEIGHT},6,270;1,1338,2,345,3,135;1,1338,2,375,3,{165+CORRIDOR_HEIGHT},6,180;1,1338,2,315,3,105;1,1338,2,375,3,165;1,1338,2,315,3,{195+CORRIDOR_HEIGHT},6,270;1,1338,2,405,3,195;1,1338,2,435,3,195,6,90;1,1338,2,405,3,{195+CORRIDOR_HEIGHT},6,180;1,1338,2,435,3,{195+CORRIDOR_HEIGHT},6,270;'
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- */
+// checks if a certain orientation pattern matches the most recent previous orientations
+// essentially this checks if the end of one int [] equals another int []
+static bool orientationMatch(int prevO[11], const std::vector<int> pattern) {
+	if(pattern.size()>11) return false;
+	for(int i = 0; i < pattern.size(); i++) {
+		if(pattern[i] != prevO[i+(11-pattern.size())]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+static void orientationShift(int prevO[11], int newO) {
+	for(int i = 0; i < 10; i++) {
+		prevO[i] = prevO[i+1];
+	}
+	prevO[10] = newO;
+}
+
+static void exportLvlStringGMD(ghc::filesystem::path const& path, std::string ld1) {
+	auto lvlData = ByteVector(ld1.begin(), ld1.end());
+	file::writeBinary(path, lvlData);
+}
+
 #include <Geode/modify/LevelBrowserLayer.hpp>
 class $modify(GenerateLevelLayer, LevelBrowserLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init(GJSearchObject* s) {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
 
-		 todo
-		 - gjsearchobject (ref gd share)
-		 - test
-		 */
+	bool init(GJSearchObject* s) {
+
 		if (!LevelBrowserLayer::init(s)) {
 			return false;
 		}
-
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons	
-		*/
+		
 		if(s->m_searchType != SearchType::MyLevels) return true;
 		auto genButton = CCMenuItemSpriteExtra::create(
 			CircleButtonSprite::createWithSpriteFrameName("waveman_s.png"_spr, .90f, CircleBaseColor::Cyan, CircleBaseSize::Medium),
 			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
 			menu_selector(GenerateLevelLayer::onGenButton)
 		);
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
 		auto menu = this->getChildByID("my-levels-menu");
 		menu->addChild(genButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
 		genButton->setID("generate-level-button"_spr);
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
 		menu->updateLayout();
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
 		return true;
-	}
-
-	// checks if a certain orientation pattern matches the most recent previous orientations
-	// essentially this checks if the end of one int [] equals another int []
-	static bool orientationMatch(int prevO[11], const std::vector<int> pattern) {
-		if(pattern.size()>11) return false;
-		for(int i = 0; i < pattern.size(); i++) {
-			if(pattern[i] != prevO[i+(11-pattern.size())]) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	static void orientationShift(int prevO[11], int newO) {
-		for(int i = 0; i < 10; i++) {
-			prevO[i] = prevO[i+1];
-		}
-		prevO[10] = newO;
 	}
 
 	static std::string mainGen() {
@@ -297,12 +247,6 @@ class $modify(GenerateLevelLayer, LevelBrowserLayer) {
 
 		if(debug) log::info("{}", level);
 
-		// deflate usage source: https://github.com/HJfod/GMD-API/blob/main/src/GMD.cpp
-		/*unsigned char* gz = nullptr;
-		int memEnd = ZipUtils::ccDeflateMemory(reinterpret_cast<unsigned char*>(level.data()), level.size(), &gz);
-		if(!memEnd) return FLAlertLayer::create("Export Error", "Level data compression failed! Please try restarting, updating JFP or reporting the issue.", "Sure thing...")->show();
-		auto levelBV = ByteVector(reinterpret_cast<uint8_t*>(gz), reinterpret_cast<uint8_t(gz + memEnd));
-		if(gz) delete*/
 		int songSelection = soundtrack[stdtr(e2)];
 		std::string b64 = ZipUtils::compressString(level, true, 0);
 		std::string desc = fmt::format("Seed: {}", seed);
@@ -324,17 +268,7 @@ class $modify(GenerateLevelLayer, LevelBrowserLayer) {
 
 	}
 
-	static void exportLvlStringGMD(ghc::filesystem::path const& path, std::string ld1) {
-		auto lvlData = ByteVector(ld1.begin(), ld1.end());
-		file::writeBinary(path, lvlData);
-	}
-
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
+	// waveman button callback -- return type here must be void and parameter must be CCObject*
 	void onGenButton(CCObject*) {
 		// Special thanks to HJFod for their work on GDShare and GMD-API, which was used as reference here
 		// https://github.com/HJfod
